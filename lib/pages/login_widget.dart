f89b7aa1cd90c133e0bdda3ac65129633693b707
kicok <kicoker@gmail.com>
2021-12-21 17:16:23 +0900

    add facebook loign

--- lib/pages/login_widget.dart
+++ lib/pages/login_widget.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 //https://firebase.flutter.dev/docs/auth/social 에서 코드 복사해옴
 class LoginWidget extends StatelessWidget {
@@ -24,6 +25,18 @@ class LoginWidget extends StatelessWidget {
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    // Once signed in, return the UserCredential
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +54,15 @@ class LoginWidget extends StatelessWidget {
                 backgroundColor: Colors.grey.withOpacity(0.3),
                 primary: Colors.black,
               ),
-            )
+            ),
+            TextButton(
+              onPressed: signInWithFacebook,
+              child: const Text('Facebook login'),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.grey.withOpacity(0.3),
+                primary: Colors.black,
+              ),
+            ),
           ],
         ),
       ),
